4971960fe9dc97e7c18f574a4cafb4078da28b34
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-09-08 15:58:28 +0200

    add function controller

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+    public function index()
+    {
+        // ASSEGNO UNA VARIABILE AL DB CON ::ALL() SELEZIONO TUTTE LE TABELLE
+        $movies = Movie::all();
+        // RITORNO CON LA FUNZIONE VIEW GLI ELEMENTI CHE BISOGNO VISUALIIZARE
+        return view("MovieController", compact("movies"));
+    }
 }
